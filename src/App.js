1d39db1d2394628fb99557555516153b881af654
aroomforimprovement <seamusart.bradley@gmail.com>
2022-04-13 20:13:37 +0100

    App and public dir

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 import React from "react";
+import { MyButton } from './lib/components/index.js';
 
 const App = () => {
 
+    return(
+        <div>
+            <MyButton btnText={'Demo button'}/>
+        </div>
+    );
+    
 }
 
 export default App;
